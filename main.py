c678573092e728432e2e83f5daf287fd9daabf91
Tower5954 <rvanna@hotmail.co.uk>
2020-11-29 21:51:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+# leo = Turtle()
+# print(leo)
+# leo.shape("turtle")
+# leo.color("chartreuse2")
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
